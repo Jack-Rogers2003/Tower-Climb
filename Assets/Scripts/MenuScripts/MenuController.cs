f203760f765adbf0d611a019f6054ea2d31d9a7b
Jack-Rogers2003 <jack.rogers200315@gmail.com>
2025-03-03 17:28:57 +0000

    Basic Menu Created
    Exit button functions and start button sends to the battle screen

---
+++ Assets/Scripts/MenuScripts/MenuController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start the game and load the GameScene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("BattleScreen", LoadSceneMode.Single);  // Load the gameplay scene
+    }
+
+    // Quit the game
+    public void ExitGame()
+    {
+        Application.Quit();  // This will close the game
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // Stop the game in the editor
+#endif
+    }
+}
